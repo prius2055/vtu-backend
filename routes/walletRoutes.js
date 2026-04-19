afbcd9edfca131ec1f942d1e288efb703b2931b3
Prius <prius2055@gmail.com>
2026-04-19 01:03:43 +0100

    still debugging

--- routes/walletRoutes.js
+++ routes/walletRoutes.js
@@ -10,7 +10,16 @@ const { protect } = require("../middleware/authMiddleware");
 router.use(protect); // Protect all wallet routes
 
 router.get("/get", protect, getWallet);
-router.post("/fund", protect, createVirtualAccount);
+router.post(
+  "/fund",
+  (req, res, next) => {
+    console.log("🔍 Body type:", typeof req.body, Buffer.isBuffer(req.body));
+    console.log("🔍 Body:", req.body);
+    next();
+  },
+  protect,
+  createVirtualAccount,
+);
 router.post("/upgrade", protect, upgradeToReseller);
 
 // router.post("/admin/update-prices", protect, restrictTo("admin"), updatePrices);
